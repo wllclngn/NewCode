f30343058041e20e33c05501684f3295f192cb13
Will C. <wu352@hotmail.com>
2025-05-29 21:47:37 -0500

    More.

--- include/Reducer_DLL_so.h
+++ include/Reducer_DLL_so.h
@@ -15,32 +15,30 @@
 #endif
 
 class DLL_so_EXPORT ReducerDLLso {
-public:
-    static constexpr size_t DEFAULT_REDUCE_MIN_THREADS = 0; 
-    static constexpr size_t DEFAULT_REDUCE_MAX_THREADS = 0; 
     static constexpr size_t FALLBACK_REDUCE_THREAD_COUNT = 2;
-
     virtual ~ReducerDLLso() {}
 
-    // Unified reduce method
     virtual void reduce(
-        const std::vector<std::pair<std::string, int>>& mappedData, 
+        const std::vector<std::pair<std::string, int>>& mappedData,
         std::map<std::string, int>& reducedData,
-        size_t minPoolThreadsConfig = DEFAULT_REDUCE_MIN_THREADS,
-        size_t maxPoolThreadsConfig = DEFAULT_REDUCE_MAX_THREADS
+        size_t minPoolThreadsConfig = 0,
+        size_t maxPoolThreadsConfig = 0
     );
 
-protected:
-    // Internal processing logic for reduce
     void process_reduce_internal(
-        const std::vector<std::pair<std::string, int>>& mappedData, 
+    const std::vector<std::pair<std::string, int>>& mappedData,
+    std::map<std::string, int>& reducedData,
+    size_t minThreads,
+    size_t maxThreads);
+
+protected:protected:
+    void process_reduce_internal(
+        const std::vector<std::pair<std::string, int>>& mappedData,
         std::map<std::string, int>& reducedData,
-        size_t minThreads, 
+        size_t minThreads,
         size_t maxThreads
     );
 
-    // Dynamic chunk size calculation
     size_t calculate_dynamic_chunk_size(size_t totalSize, size_t guideMaxThreads = 0) const;
 };
-
 #endif // REDUCER_DLL_SO_H
